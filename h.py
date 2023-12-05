d26eac894ca544d9d86026947d7bdcaf3f54c766
Yugma-p <kunjuyugma@gmail.com>
2023-12-05 10:05:37 +0530

    python 5 th dec

---
+++ h.py
@@ -0,0 +1,3 @@
+s=input("enter a word")
+print(s[-1]+s[1:-1]+s[0])
+
---
+++ sum.py
@@ -0,0 +1,16 @@
+li=[6,5,3,8,4,2,5,11]
+sum=0
+for item in li:
+    sum=sum+item
+print("The sum is:",sum)
+
+for H in "PROGRAM":
+    print("character:",H)
+
+print(range(10))
+print(list(range(10)))
+
+gen=['pop','lenovo','apple']
+for i in range(len(gen)):
+      print("i like:",gen[i])
+
